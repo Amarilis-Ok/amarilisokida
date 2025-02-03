b283de50767041a78fac026baef5f0d26cf90880
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2025-02-03 16:44:54 -0300

    Updates

---
+++ precache-manifest.b3da8bd2372c354d6a27d656a8d5eec9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a46f30ff59631ffd627",
+    "url": "/amarilisokida/static/css/main.54855c63.chunk.css"
+  },
+  {
+    "revision": "5a46f30ff59631ffd627",
+    "url": "/amarilisokida/static/js/main.5a46f30f.chunk.js"
+  },
+  {
+    "revision": "5933624ef5c4c436412d",
+    "url": "/amarilisokida/static/js/1.5933624e.chunk.js"
+  },
+  {
+    "revision": "5d48692cff774ab301dc",
+    "url": "/amarilisokida/static/css/2.9382db08.chunk.css"
+  },
+  {
+    "revision": "5d48692cff774ab301dc",
+    "url": "/amarilisokida/static/js/2.5d48692c.chunk.js"
+  },
+  {
+    "revision": "81ba38e2a0000145967f",
+    "url": "/amarilisokida/static/js/runtime~main.81ba38e2.js"
+  },
+  {
+    "revision": "539d2fe17b3ee5825effdab01b29bdb5",
+    "url": "/amarilisokida/static/media/Amarilis_logo_final__area_entorno.539d2fe1.jpg"
+  },
+  {
+    "revision": "04260433aaf0803a5d353cd6fbcfdc58",
+    "url": "/amarilisokida/static/media/photo.04260433.jpeg"
+  },
+  {
+    "revision": "3a2f8f8beeef9ef86a0b99b1a9230106",
+    "url": "/amarilisokida/static/media/saopaulo.3a2f8f8b.jpeg"
+  },
+  {
+    "revision": "db8a82a803f34ad657b888ff8a9360d6",
+    "url": "/amarilisokida/static/media/teacher.db8a82a8.jpeg"
+  },
+  {
+    "revision": "4aa700dbb6b64260d106534db5cb3c38",
+    "url": "/amarilisokida/static/media/interpret2b.4aa700db.jpeg"
+  },
+  {
+    "revision": "5a628529f03223b4340590af36717596",
+    "url": "/amarilisokida/static/media/book.5a628529.jpeg"
+  },
+  {
+    "revision": "ced717e95fbdc2a3532ff5a698078639",
+    "url": "/amarilisokida/static/media/games.ced717e9.jpeg"
+  },
+  {
+    "revision": "d960c4eb88e00208acc81babd23cbca5",
+    "url": "/amarilisokida/static/media/selo.d960c4eb.jpeg"
+  },
+  {
+    "revision": "4f6ef375bc91392581bac14a51b2dae3",
+    "url": "/amarilisokida/static/media/Berenice.4f6ef375.png"
+  },
+  {
+    "revision": "1ab5f4fa2298f400527bf3c2497c3e61",
+    "url": "/amarilisokida/static/media/cordel_2_.1ab5f4fa.jpg"
+  },
+  {
+    "revision": "1c385a58baa17bf1867dc437eb9580be",
+    "url": "/amarilisokida/static/media/photographer.1c385a58.webp"
+  },
+  {
+    "revision": "7fbe07865c2cfcc25eaf47dedf1be75b",
+    "url": "/amarilisokida/static/media/waiting.7fbe0786.jpg"
+  },
+  {
+    "revision": "af3a1d9ad9259a99b46bf8299a5bc848",
+    "url": "/amarilisokida/static/media/100_facts.af3a1d9a.jpg"
+  },
+  {
+    "revision": "af6fba48271a742c436ef0f2519644ac",
+    "url": "/amarilisokida/static/media/ella.af6fba48.jpg"
+  },
+  {
+    "revision": "c8528479e596e8ddf30f38ee0874877d",
+    "url": "/amarilisokida/static/media/ghost_in_the_attic.c8528479.jpg"
+  },
+  {
+    "revision": "3852791c5a068d6a35bff1dd06d942c9",
+    "url": "/amarilisokida/static/media/haunted_library.3852791c.jpg"
+  },
+  {
+    "revision": "6d5fe6d4ca7cbc80f22f4a972c6fdd21",
+    "url": "/amarilisokida/static/media/human_body.6d5fe6d4.jpg"
+  },
+  {
+    "revision": "d9e1fb0d29d396638709661bc9e75fa3",
+    "url": "/amarilisokida/static/media/jesus.d9e1fb0d.webp"
+  },
+  {
+    "revision": "e6c88accdf9713cca8edeb3a43daa398",
+    "url": "/amarilisokida/static/media/numeros_de_azar.e6c88acc.jpg"
+  },
+  {
+    "revision": "51eaff961168c8071f87f7f40b2a4a2c",
+    "url": "/amarilisokida/static/media/peixe_zumbi.51eaff96.jpg"
+  },
+  {
+    "revision": "4a678c684bfe44ef49a81c8a7189045c",
+    "url": "/amarilisokida/static/media/r.4a678c68.webp"
+  },
+  {
+    "revision": "63d5333b4d1007f2efaeaaf12132071c",
+    "url": "/amarilisokida/index.html"
+  }
+];
